88765877777d20f22d3c279fff93ee2f826eb611
lucasmagalhaes33 <lucas.magalhaes911@gmail.com>
2022-03-21 18:50:53 -0300

    first commit

---
+++ src/main/java/com/lucas/crudspring/repository/CourseRepository.java
@@ -0,0 +1,11 @@
+package com.lucas.crudspring.repository;
+
+import com.lucas.crudspring.model.Course;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+    
+}
